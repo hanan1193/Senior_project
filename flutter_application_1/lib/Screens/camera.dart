d13d80233a6209c91bd918de9ca4d071724eee10
hanan <hanan.nairat13@gmail.com>
2025-03-29 02:33:01 +0300

    change to camera and clock file

--- flutter_application_1/lib/Screens/camera.dart
+++ flutter_application_1/lib/Screens/camera.dart
@@ -9,8 +9,32 @@ class CameraSereen extends StatefulWidget {
 class _CameraSereenState extends State<CameraSereen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Camera ......'),
+   return Scaffold(
+      appBar: AppBar(
+        title: Text("Drowning Detector"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            // Camera preview placeholder
+             SizedBox(height: 30),
+            Container(
+              height: 100,
+              margin: EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+              ),
+              child: Center(child: Text("Camera View")),
+            ),
+             Text("Drowning or safe", style: TextStyle(
+                        color:Colors.redAccent,
+                        fontSize: 16))
+            
+            // Drowning Detector Button
+          ],
+        ),
+      ),
     );
   }
 }
--- flutter_application_1/lib/Screens/clock.dart
+++ flutter_application_1/lib/Screens/clock.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 class ClockScreen extends StatefulWidget {
@@ -10,8 +11,49 @@ class ClockScreen extends StatefulWidget {
 class _ClockScreenState extends State<ClockScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Clock.....'),
+    return  Scaffold(
+      appBar: AppBar(title: Text("Health Monitor"),centerTitle: true,),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/f1.jpeg"), 
+                  fit: BoxFit.cover, 
+                ),
+              ),
+            ),
+          ),
+          
+          Positioned(
+            top: 50,
+            right: 20,
+            child: Card(
+              elevation: 6,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                     Text("ID:100002", style: TextStyle(fontSize: 16)),
+                    Text("Heart Rate: 80 BPM", style: TextStyle(fontSize: 16)),
+                    Text("SPO2: 98%", style: TextStyle(fontSize: 16)),
+                    Text("Acceleration: Normal", style: TextStyle(fontSize: 16)),
+                     Text("Drowning or safe", style: TextStyle(
+                      color:Colors.redAccent,
+                      fontSize: 16)),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
+
